e8ab584bceba6426a4449f46642b7bf5b0344ad4
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2021-12-29 09:23:09 +0100

    working on iOS

---
+++ ios/FilterTheWorld/FilterTheWorld/bridge/FilterTheWorld-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#ifndef FilterTheWorld_Bridging_Header_h
+#define FilterTheWorld_Bridging_Header_h
+
+#include "DemoViewController.h"
+
+#endif /* FilterTheWorld_Bridging_Header_h */
