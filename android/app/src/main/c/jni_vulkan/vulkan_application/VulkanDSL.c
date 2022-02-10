8325f0aa662f381018f6a5efa247da5c8699334d
Thierry Vilmart <thierry@getwemap.com>
2022-02-10 23:41:40 +0100

    Removes the static image for testing
    This reverts commit b5ca3a70bbd6373d3905e0af4b25e82495c34463.

--- android/app/src/main/c/jni_vulkan/vulkan_application/VulkanDSL.c
+++ android/app/src/main/c/jni_vulkan/vulkan_application/VulkanDSL.c
@@ -3439,7 +3439,7 @@ void vulkanDSL_main(struct VulkanDSL *vulkanDSL) {
 
     demo_prepare(vulkanDSL);
 
-    demo_draw(vulkanDSL, 1.3);
+    demo_draw(vulkanDSL, 0);
 }
 
 void VulkanDSL__setSize(struct VulkanDSL *vulkanDSL, int32_t width, int32_t height) {
